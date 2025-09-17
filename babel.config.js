b0899dd17b482f5d3194184b7b3a35f54ac4fbd7
Marco <mf681175@gmail.com>
2025-09-17 14:00:09 -0600

    ok

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,15 @@
-module.exports = {
+/* module.exports = {
   presets: ["babel-preset-expo"],
   plugins: ["react-native-reanimated/plugin"], // 👈 debe ir al final siempre
+}; */
+
+
+module.exports = function(api) {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+    plugins: [
+      'react-native-reanimated/plugin', // ✅ mantener así
+    ],
+  };
 };
